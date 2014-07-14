6efbbd182c1814ce209805a8224cd9c9ea0a4125
Vivian Huang <vivian.huang@airbnb.com>
2014-07-13 17:41:50 -0700

    Initial Commit

---
+++ PennMobile/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  PennMobile
+//
+//  Created by Vivian Huang on 7/13/14.
+//  Copyright (c) 2014 PennLabs. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+                            
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
