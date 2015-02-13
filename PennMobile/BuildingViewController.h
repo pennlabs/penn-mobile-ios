4c0083b48fa7ae191cf8e68a66fa7eaee474a7cf
Sacha Best <sachabest@outlook.com>
2015-02-12 22:43:18 -0500

    Adding Building view with centering on user loc

---
+++ PennMobile/BuildingViewController.h
@@ -0,0 +1,19 @@
+//
+//  BuildingViewController.h
+//  PennMobile
+//
+//  Created by Sacha Best on 2/12/15.
+//  Copyright (c) 2015 PennLabs. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <MapKit/MapKit.h>
+
+@interface BuildingViewController : UIViewController <MKMapViewDelegate, UISearchBarDelegate, CLLocationManagerDelegate>
+
+@property (weak, nonatomic) IBOutlet MKMapView *mapView;
+@property (weak, nonatomic) IBOutlet UISearchBar *searchBar;
+
+@property(nonatomic, retain) CLLocationManager *locationManager;
+
+@end
---
+++ PennMobile/BuildingViewController.m
@@ -0,0 +1,73 @@
+//
+//  BuildingViewController.m
+//  PennMobile
+//
+//  Created by Sacha Best on 2/12/15.
+//  Copyright (c) 2015 PennLabs. All rights reserved.
+//
+
+#import "BuildingViewController.h"
+
+@interface BuildingViewController ()
+
+@end
+
+@implementation BuildingViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+    self.locationManager = [[CLLocationManager alloc] init];
+    self.locationManager.delegate = self;
+    [self.locationManager startUpdatingLocation];
+    if(IS_OS_8_OR_LATER) {
+        // Use one or the other, not both. Depending on what you put in info.plist
+        [self.locationManager requestWhenInUseAuthorization];
+    }
+    _mapView.showsUserLocation = YES;
+    [_mapView setMapType:MKMapTypeStandard];
+    [_mapView setZoomEnabled:YES];
+    [_mapView setScrollEnabled:YES];
+}
+-(void)viewDidAppear:(BOOL)animated {
+    [super viewDidAppear:animated];
+    self.locationManager.distanceFilter = kCLDistanceFilterNone;
+    self.locationManager.desiredAccuracy = kCLLocationAccuracyBest;
+    [self.locationManager startUpdatingLocation];
+    
+    [self centerMapOnLocation];
+}
+- (void)centerMapOnLocation {
+    //View Area
+    MKCoordinateRegion region = { { 0.0, 0.0 }, { 0.0, 0.0 } };
+    region.center.latitude = self.locationManager.location.coordinate.latitude;
+    region.center.longitude = self.locationManager.location.coordinate.longitude;
+    region.span.longitudeDelta = 0.005f;
+    region.span.longitudeDelta = 0.005f;
+    [_mapView setRegion:region animated:YES];
+}
+-(void)locationManager:(CLLocationManager *)manager didUpdateLocations:(NSArray *)locations {
+    [self centerMapOnLocation];
+}
+- (void)locationManager:(CLLocationManager *)manager didChangeAuthorizationStatus:(CLAuthorizationStatus)status {
+    
+    if (status == kCLAuthorizationStatusAuthorizedWhenInUse) {
+        self.mapView.showsUserLocation = YES;
+    }
+}
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
