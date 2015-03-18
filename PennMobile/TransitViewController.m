f56326d774cbda327af3ccc265647f58d215666e
Sacha Best <sachabest@outlook.com>
2015-03-17 22:35:54 -0400

    Framework for New Transit view (including places)

--- PennMobile/TransitViewController.m
+++ PennMobile/TransitViewController.m
@@ -17,25 +17,149 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view.
-    NSURLRequest *req = [NSURLRequest requestWithURL:[NSURL URLWithString:@"http://www.pennrides.com/map?showHeader=0&route=229,230&silent_disable_timeout=1"]];
-    [_webView loadRequest:req];
-    _webView.scalesPageToFit = NO;
-    _webView.delegate = self;
-}
-- (void)webViewDidStartLoad:(UIWebView *)webView {
-    [MBProgressHUD showHUDAddedTo:self.view animated:YES];
-}
-
-- (void)webViewDidFinishLoad:(UIWebView *)webView {
-    if (!webView.loading) {
-        [MBProgressHUD hideAllHUDsForView:self.view animated:YES];
+    locationManager = [[CLLocationManager alloc] init];
+    locationManager.delegate = self;
+    [locationManager startUpdatingLocation];
+    if(IS_OS_8_OR_LATER) {
+        // Use one or the other, not both. Depending on what you put in info.plist
+        [locationManager requestWhenInUseAuthorization];
     }
+    _mapView.showsUserLocation = YES;
+    [_mapView setMapType:MKMapTypeStandard];
+    [_mapView setZoomEnabled:YES];
+    [_mapView setScrollEnabled:YES];
+    // Do any additional setup after loading the view.
+}
+- (void)viewDidAppear:(BOOL)animated {
+    locationManager.distanceFilter = kCLDistanceFilterNone;
+    locationManager.desiredAccuracy = kCLLocationAccuracyBest;
+    shouldCenter = YES;
+    [locationManager startUpdatingLocation];
 }
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
 }
+#pragma mark - PennUber API
 
+-(NSArray *)queryAPI:(double)latFrom latTo:(double)latTo lonFrom:(double)lonFrom lonTo:(double)lonTo {
+    
+    NSURL *url = [NSURL URLWithString:[NSString stringWithFormat:@"%@%@latFrom=%f&latTo=%f&lonFrom=%f&lonTo=%f", SERVER_ROOT, TRANSIT_PATH, latFrom, latTo, lonFrom, lonTo ]];
+    NSData *result = [NSData dataWithContentsOfURL:url];
+    if (![self confirmConnection:result]) {
+        return nil;
+    }
+    NSError *error;
+    if (!result) {
+        //CLS_LOG(@"Data parameter was nil for query..returning null");
+        return nil;
+    }
+    NSDictionary *returned = [NSJSONSerialization JSONObjectWithData:result options:NSJSONReadingMutableLeaves error:&error];
+    if (error) {
+        [NSException raise:@"JSON parse error" format:@"%@", error];
+    }
+    return returned[@"result_data"];
+}
+
+- (BOOL)confirmConnection:(NSData *)data {
+    if (!data) {
+        UIAlertView *new = [[UIAlertView alloc] initWithTitle:@"Couldn't Connect to API" message:@"We couldn't connect to Penn's API. Please try again later. :(" delegate:self cancelButtonTitle:@"OK" otherButtonTitles:nil, nil];
+        [new show];
+        return false;
+    }
+    return true;
+}
+
+#pragma mark - Searching and Plotting
+
+- (void)plotResults {
+    [_mapView removeAnnotations:_mapView.annotations];
+    MKPointAnnotation *temp;
+    if (results.count > 0) {
+        for (long i = results.count - 1; i >= 0; i--) {
+            temp = [[MKPointAnnotation alloc] init];
+            temp.coordinate = ((MKMapItem *) results[i]).placemark.coordinate;
+            temp.title = ((MKMapItem *) results[i]).name;
+            // these values were originally store in a local hashmap but wasn't worth it
+            [_mapView addAnnotation:temp];
+        }
+        [_mapView selectAnnotation:temp animated:YES];
+    }
+    
+}
+
+/**
+ * The majority of this code is from Apple's samples. Just a heads up
+ **/
+- (void)search:(NSString *)query {
+    MKCoordinateRegion newRegion;
+    newRegion.center.latitude = locationManager.location.coordinate.latitude;
+    newRegion.center.longitude = locationManager.location.coordinate.longitude;
+    
+    // setup the area spanned by the map region:
+    // we use the delta values to indicate the desired zoom level of the map,
+    //      (smaller delta values corresponding to a higher zoom level)
+    //
+    newRegion.span.latitudeDelta = 0.112872;
+    newRegion.span.longitudeDelta = 0.109863;
+    
+    MKLocalSearchRequest *request = [[MKLocalSearchRequest alloc] init];
+    
+    request.naturalLanguageQuery = query;
+    request.region = newRegion;
+    
+    MKLocalSearchCompletionHandler completionHandler = ^(MKLocalSearchResponse *response, NSError *error) {
+        if (error != nil) {
+            NSString *errorStr = [[error userInfo] valueForKey:NSLocalizedDescriptionKey];
+            UIAlertView *alert = [[UIAlertView alloc] initWithTitle:@"Could not find any results."
+                                                            message:errorStr
+                                                           delegate:nil
+                                                  cancelButtonTitle:@"OK"
+                                                  otherButtonTitles:nil];
+            [alert show];
+        }
+        else {
+            results = [response mapItems];
+            [MBProgressHUD hideHUDForView:self.view animated:YES];
+            [self plotResults];
+            // used for later when setting the map's region in "prepareForSegue"
+            _boundingRegion = response.boundingRegion;
+        }
+        //[UIApplication sharedApplication].networkActivityIndicatorVisible = NO;
+    };
+    MKLocalSearch *localSearch = [[MKLocalSearch alloc] initWithRequest:request];
+    [MBProgressHUD showHUDAddedTo:self.view animated:YES];
+    [localSearch startWithCompletionHandler:completionHandler];
+}
+#pragma mark - UISearchBarDelegate
+
+- (void)searchBarSearchButtonClicked:(UISearchBar *)searchBar {
+    [self search:searchBar.text];
+    shouldCenter = NO;
+}
+
+#pragma mark - CLLocationManager
+
+-(void)locationManager:(CLLocationManager *)manager didUpdateLocations:(NSArray *)locations {
+    if (shouldCenter)
+        [self centerMapOnLocation];
+}
+- (void)locationManager:(CLLocationManager *)manager didChangeAuthorizationStatus:(CLAuthorizationStatus)status {
+    
+    if (status == kCLAuthorizationStatusAuthorizedWhenInUse) {
+        self.mapView.showsUserLocation = YES;
+    }
+}
+
+- (void)centerMapOnLocation {
+    //View Area
+    MKCoordinateRegion region = { { 0.0, 0.0 }, { 0.0, 0.0 } };
+    region.center.latitude = locationManager.location.coordinate.latitude;
+    region.center.longitude = locationManager.location.coordinate.longitude;
+    region.span.longitudeDelta = 0.005f;
+    region.span.longitudeDelta = 0.005f;
+    [_mapView setRegion:region animated:YES];
+}
 
 #pragma mark - Navigation
 /**
@@ -60,7 +184,7 @@
         cancelTouches.numberOfTouchesRequired = 1;
         if (self.view.gestureRecognizers.count > 0) {
             // there is a keybaord dismiss tap recognizer present
-            // ((UIGestureRecognizer *) self.view.gestureRecognizers[0]).enabled = NO;
+            // ((UIGestureRecognizer *) view.gestureRecognizers[0]).enabled = NO;
         }
         float width = [[UIScreen mainScreen] bounds].size.width;
         float height = [[UIScreen mainScreen] bounds].size.height;
