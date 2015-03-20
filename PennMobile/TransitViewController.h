015f74a1a33035d49bc7dfb24ae48dc071121301
Sacha Best <sachabest@outlook.com>
2015-03-19 21:22:01 -0400

    Added basic UI for routing, polylines from GMaps

--- PennMobile/TransitViewController.h
+++ PennMobile/TransitViewController.h
@@ -22,6 +22,14 @@
 }
 
 @property (weak, nonatomic) IBOutlet MKMapView *mapView;
+@property (weak, nonatomic) IBOutlet UISearchBar *searchBar;
+@property (weak, nonatomic) IBOutlet UILabel *labelDestination;
+@property (weak, nonatomic) IBOutlet UILabel *labelRouteName;
+@property (weak, nonatomic) IBOutlet UILabel *labelEnd;
+@property (weak, nonatomic) IBOutlet UILabel *labelStart;
+@property (weak, nonatomic) IBOutlet UILabel *labelWalkEnd;
+@property (weak, nonatomic) IBOutlet UILabel *labelWalkStart;
+
 @property (nonatomic, assign) MKCoordinateRegion boundingRegion;
 
 - (void)search:(NSString *)query;
