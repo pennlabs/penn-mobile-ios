2fbf3cb2e1cdca7a499de3657012694771d85397
Alice <aliceren@seas.upenn.edu>
2015-04-08 22:30:04 -0400

    remove Pods directory; update .gitignore

--- Pods/Target Support Files/Pods-DropdownMenu/Pods-DropdownMenu-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_DropdownMenu : NSObject
-@end
-@implementation PodsDummy_Pods_DropdownMenu
-@end
--- Pods/Target Support Files/Pods-HMLauncherView/Pods-HMLauncherView-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_HMLauncherView : NSObject
-@end
-@implementation PodsDummy_Pods_HMLauncherView
-@end
