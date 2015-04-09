32a2a1bd1851a1df6b3bc493784631547b0c5e05
Alice <aliceren@seas.upenn.edu>
2015-04-08 21:57:25 -0400

    PennLabs logo fades with scroll; fix #35
    also update contact email for feature request

--- PennMobile/SlideOutMenuViewController.m
+++ PennMobile/SlideOutMenuViewController.m
@@ -90,6 +90,49 @@ static SlideOutMenuViewController *instance;
 - (void)returnToView {
     [self performSegueWithIdentifier:currentView sender:self];
 }
+
+#pragma mark - UIScrollViewDelegate methods
+- (void)scrollViewDidScroll:(UIScrollView *)scrollView {
+    double animDuration = 0.8;
+    
+    if (_lastScrollOffset > scrollView.contentOffset.y && scrollView.contentOffset.y <= scrollView.contentSize.height) {
+        // scrolled up
+        NSLog(@"Up");
+        
+        [UIView beginAnimations:@"fade in" context:nil];
+        [UIView setAnimationDuration:animDuration];
+        _labsImage.alpha = 1.0;
+        [UIView commitAnimations];
+    } else if (_lastScrollOffset < scrollView.contentOffset.y) {
+        // scrolled down
+        NSLog(@"Down");
+        
+        [UIView beginAnimations:@"fade out" context:nil];
+        [UIView setAnimationDuration:animDuration];
+        _labsImage.alpha = 0.0;
+        [UIView commitAnimations];
+    } else if (scrollView.contentOffset.y == 0) {
+        [UIView beginAnimations:@"fade in" context:nil];
+        [UIView setAnimationDuration:animDuration/2];
+        _labsImage.alpha = 1.0;
+        [UIView commitAnimations];
+    }
+    
+    _lastScrollOffset = scrollView.contentOffset.x;
+}
+
+- (void)scrollViewDidEndDecelerating:(UIScrollView *)scrollView {
+    // stopped scrolling
+}
+
+- (void)scrollViewDidEndDragging:(UIScrollView *)scrollView
+                  willDecelerate:(BOOL)decelerate {
+    if (!decelerate) {
+        // stopped scrolling
+    }
+}
+
+
 #pragma mark - Navigation
 
 // In a storyboard-based application, you will often want to do a little preparation before navigation
