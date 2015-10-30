f7b4a8a23f04e6267d8ae4fb80073d949aeb1de5
kbharathala <kbharathala@gmail.com>
2015-10-30 17:29:18 -0400

    Changed image file and added laundry class

---
+++ PennMobile/LaundryTableViewController.h
@@ -0,0 +1,13 @@
+//
+//  LaundryTableViewController.h
+//  PennMobile
+//
+//  Created by Krishna Bharathala on 10/30/15.
+//  Copyright © 2015 PennLabs. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface LaundryTableViewController : UITableViewController
+
+@end
---
+++ PennMobile/LaundryTableViewController.m
@@ -0,0 +1,62 @@
+//
+//  LaundryTableViewController.m
+//  PennMobile
+//
+//  Created by Krishna Bharathala on 10/30/15.
+//  Copyright © 2015 PennLabs. All rights reserved.
+//
+
+#import "LaundryTableViewController.h"
+
+@interface LaundryTableViewController ()
+
+@end
+
+@implementation LaundryTableViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    
+    // Uncomment the following line to preserve selection between presentations.
+    // self.clearsSelectionOnViewWillAppear = NO;
+    
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+#pragma mark - Table view data source
+
+- (NSInteger)numberOfSectionsInTableView:(UITableView *)tableView {
+#warning Incomplete implementation, return the number of sections
+    return 0;
+}
+
+- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section {
+#warning Incomplete implementation, return the number of rows
+    return 0;
+}
+
+/*
+- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath {
+    UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:<#@"reuseIdentifier"#> forIndexPath:indexPath];
+    
+    // Configure the cell...
+    
+    return cell;
+}
+*/
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
