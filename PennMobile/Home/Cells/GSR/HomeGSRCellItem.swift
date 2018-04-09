bfccabe624dc27a0375689784f26a5022ff1f8b5
dom <hello@dom.voyage>
2018-04-08 23:05:42 -0400

    added support for the differentiation of WIC booths and rooms

--- PennMobile/Home/Cells/GSR/HomeGSRCellItem.swift
+++ PennMobile/Home/Cells/GSR/HomeGSRCellItem.swift
@@ -21,7 +21,7 @@ final class HomeGSRCellItem: HomeCellItem {
     init() {
     }
     
-    var bookingOptions: [(StudyRoomBookingOption?, StudyRoomBookingOption?, StudyRoomBookingOption?)]?
+    var bookingOptions: [(GSRTimeSlot?, GSRTimeSlot?, GSRTimeSlot?)]?
     
     func equals(item: HomeCellItem) -> Bool {
         guard let _ = item as? HomeGSRCellItem else { return false }
@@ -62,7 +62,7 @@ extension HomeGSRCellItem: HomeAPIRequestable {
         formatter.dateFormat = "yyyy-MM-dd"
         let dateString = formatter.string(from: Date())
         
-        GSRNetworkManager.instance.getAvailability(for: 1086, dateStr: "2018-04-9") { (rooms) in
+        GSRNetworkManager.instance.getAvailability(for: 1086, dateStr: "2018-04-10") { (rooms) in
             if let _ = rooms {
                 self.filterForTimeConstraints(rooms!)
             }
@@ -71,23 +71,37 @@ extension HomeGSRCellItem: HomeAPIRequestable {
     }
     
     private func filterForTimeConstraints(_ rooms: [GSRRoom]) {
-        let wicRooms = rooms.filter { $0.gid == 1889 }
+        let wicRooms = rooms.filter { $0.gid == 1889 && $0.capacity == 8 }
+        let wicBooths = rooms.filter { $0.gid == 1889 && $0.capacity == 6 }
+        //let wicRooms = rooms
+        //let wicBooths = rooms
+        
         let wicRoomsWith60 = wicRooms.filter { $0.timeSlots.first60 != nil }
         let wicRoomsWith90 = wicRooms.filter { $0.timeSlots.first90 != nil }
+        let wicBoothsWith60 = wicBooths.filter { $0.timeSlots.first60 != nil }
+        let wicBoothsWith90 = wicBooths.filter { $0.timeSlots.first90 != nil }
         
         print("WIC Rooms with 30: \(wicRooms.count)")
         print("WIC Rooms with 60: \(wicRoomsWith60.count)")
         print("WIC Rooms with 90: \(wicRoomsWith90.count)")
+        print("WIC Booths with 30: \(wicBooths.count)")
+        print("WIC Booths with 60: \(wicBoothsWith60.count)")
+        print("WIC Booths with 90: \(wicBoothsWith90.count)")
 
-        let first30TimeSlot = getFirst30(wicRooms)
-        let first60TimeSlot = getFirst60(wicRoomsWith60)
-        let first90TimeSlot = getFirst90(wicRoomsWith90)
+        let firstRoomSlot30 = getFirst30(wicRooms)
+        let firstRoomSlot60 = getFirst60(wicRoomsWith60)
+        let firstRoomSlot90 = getFirst90(wicRoomsWith90)
+        let firstBoothSlot30 = getFirst30(wicBooths)
+        let firstBoothSlot60 = getFirst60(wicBoothsWith60)
+        let firstBoothSlot90 = getFirst90(wicBoothsWith90)
         
+        self.bookingOptions = [(firstRoomSlot30, firstRoomSlot60, firstRoomSlot90),
+                               (firstBoothSlot30, firstBoothSlot60, firstBoothSlot90)]
     }
     
     private func getFirst30(_ rooms: [GSRRoom]) -> GSRTimeSlot? {
         let first30TimeSlot : GSRTimeSlot? = rooms.min()?.timeSlots.first
-        print(first30TimeSlot?.getLocalTimeString())
+        print(first30TimeSlot?.getLocalTimeString() ?? "no slots availible")
         return first30TimeSlot
     }
     
@@ -99,7 +113,7 @@ extension HomeGSRCellItem: HomeAPIRequestable {
             return lhsFirst60.startTime <= rhsFirst60.startTime
         }
         let first60TimeSlot : GSRTimeSlot? = first60Room?.timeSlots.first60
-        print(first60TimeSlot?.getLocalTimeString())
+        print(first60TimeSlot?.getLocalTimeString() ?? "no slots availible")
         return first60TimeSlot
     }
     
@@ -111,7 +125,7 @@ extension HomeGSRCellItem: HomeAPIRequestable {
             return lhsFirst90.startTime <= rhsFirst90.startTime
         }
         let first90TimeSlot : GSRTimeSlot? = first90Room?.timeSlots.first90
-        print(first90TimeSlot?.getLocalTimeString())
+        print(first90TimeSlot?.getLocalTimeString() ?? "no slots availible")
         return first90TimeSlot
     }
 }
