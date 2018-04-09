992d195312b120789542bd87a21b9c60636aba98
dom <hello@dom.voyage>
2018-04-08 22:32:54 -0400

    finds the first time slots in WIC for 30, 60, 90 min increments

--- PennMobile/Home/Cells/GSR/HomeGSRCellItem.swift
+++ PennMobile/Home/Cells/GSR/HomeGSRCellItem.swift
@@ -62,7 +62,7 @@ extension HomeGSRCellItem: HomeAPIRequestable {
         formatter.dateFormat = "yyyy-MM-dd"
         let dateString = formatter.string(from: Date())
         
-        GSRNetworkManager.instance.getAvailability(for: 1086, dateStr: "2018-04-10") { (rooms) in
+        GSRNetworkManager.instance.getAvailability(for: 1086, dateStr: "2018-04-9") { (rooms) in
             if let _ = rooms {
                 self.filterForTimeConstraints(rooms!)
             }
@@ -80,6 +80,8 @@ extension HomeGSRCellItem: HomeAPIRequestable {
         print("WIC Rooms with 90: \(wicRoomsWith90.count)")
 
         let first30TimeSlot = getFirst30(wicRooms)
+        let first60TimeSlot = getFirst60(wicRoomsWith60)
+        let first90TimeSlot = getFirst90(wicRoomsWith90)
         
     }
     
@@ -89,11 +91,27 @@ extension HomeGSRCellItem: HomeAPIRequestable {
         return first30TimeSlot
     }
     
-    private func getFirst60(_ rooms: [GSRRoom]) {
-        //let first60TimeSlot : GSRTimeSlot?
+    private func getFirst60(_ rooms: [GSRRoom]) -> GSRTimeSlot? {
+        let first60Room : GSRRoom? = rooms.min { (lhs, rhs) -> Bool in
+            guard let lhsFirst60 = lhs.timeSlots.first60, let rhsFirst60 = rhs.timeSlots.first60 else {
+                return false
+            }
+            return lhsFirst60.startTime <= rhsFirst60.startTime
+        }
+        let first60TimeSlot : GSRTimeSlot? = first60Room?.timeSlots.first60
+        print(first60TimeSlot?.getLocalTimeString())
+        return first60TimeSlot
     }
     
-    private func getFirst90() {
-        
+    private func getFirst90(_ rooms: [GSRRoom]) -> GSRTimeSlot? {
+        let first90Room : GSRRoom? = rooms.min { (lhs, rhs) -> Bool in
+            guard let lhsFirst90 = lhs.timeSlots.first90, let rhsFirst90 = rhs.timeSlots.first90 else {
+                return false
+            }
+            return lhsFirst90.startTime <= rhsFirst90.startTime
+        }
+        let first90TimeSlot : GSRTimeSlot? = first90Room?.timeSlots.first90
+        print(first90TimeSlot?.getLocalTimeString())
+        return first90TimeSlot
     }
 }
