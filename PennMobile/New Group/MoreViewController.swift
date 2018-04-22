7166d7859c8cba8454d27bb44ff43574e58c401c
zhileiz <zhileiz@seas.upenn.edu>
2018-04-22 19:09:28 -0400

    added more links to morecontroller

--- PennMobile/New Group/MoreViewController.swift
+++ PennMobile/New Group/MoreViewController.swift
@@ -27,14 +27,15 @@ class MoreViewController: GenericTableViewController {
         tableView.separatorStyle = .none
         //tableView.register(AccountCell.self, forCellReuseIdentifier: "account")
         tableView.register(MoreCell.self, forCellReuseIdentifier: "more")
+        tableView.register(LinkCell.self, forCellReuseIdentifier: "link")
     }
     
 }
 
 extension MoreViewController {
     override func numberOfSections(in tableView: UITableView) -> Int {
-        //return 2
-        return 1
+        //  return 3
+        return 2
     }
     
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
@@ -43,25 +44,44 @@ extension MoreViewController {
         } else {
             return ControllerModel.shared.moreOrder.count
         }*/
-        return ControllerModel.shared.moreOrder.count
+        if (section == 0) {
+            return ControllerModel.shared.moreOrder.count
+        }
+        return LinkModel.shared.links.count
     }
     
     override func tableView(_ tableView: UITableView, viewForHeaderInSection section: Int) -> UIView? {
         let headerView = HeaderViewCell()
-        headerView.setUpView(title: "FEATURES")
+        if (section == 0) {
+            headerView.setUpView(title: "FEATURES")
+        } else {
+            headerView.setUpView(title: "OTHER LINKS")
+        }
         return headerView
     }
     
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "more") as! MoreCell
-        cell.backgroundColor = .white
-        cell.setUpView(page: ControllerModel.shared.moreOrder[indexPath.row], icon: ControllerModel.shared.moreIcons[indexPath.row])
+        if (indexPath.section == 0) {
+            let cell = tableView.dequeueReusableCell(withIdentifier: "more") as! MoreCell
+            cell.backgroundColor = .white
+            cell.setUpView(page: ControllerModel.shared.moreOrder[indexPath.row], icon: ControllerModel.shared.moreIcons[indexPath.row])
+            setUpAccecories(for: cell)
+            return cell
+        } else {
+            let cell = tableView.dequeueReusableCell(withIdentifier: "link") as! LinkCell
+            cell.backgroundColor = .white
+            cell.setUpView(link: LinkModel.shared.links[indexPath.row])
+            setUpAccecories(for: cell)
+            return cell
+        }
+    }
+    
+    private func setUpAccecories(for cell: UITableViewCell) {
         let separatorHeight = CGFloat(2)
         let customSeparator = UIView(frame: CGRect(x: 0, y: cell.frame.size.height + 3 + separatorHeight, width: UIScreen.main.bounds.width, height: separatorHeight))
         customSeparator.backgroundColor = UIColor(red:0.96, green:0.97, blue:0.97, alpha:1.0)
         cell.addSubview(customSeparator)
         cell.accessoryType = .disclosureIndicator
-        return cell
     }
     
     override func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
@@ -73,10 +93,23 @@ extension MoreViewController {
     }
     
     override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
-        //if (indexPath.section == 1) {
+        if (indexPath.section == 0) {
             let targetController = ControllerModel.shared.viewController(for: ControllerModel.shared.moreOrder[indexPath.row])
             navigationController?.pushViewController(targetController, animated: true)
-        //}
+        } else {
+            popUpAndGoTo(link: LinkModel.shared.links[indexPath.row])
+        }
         tableView.deselectRow(at: indexPath, animated: true)
     }
+    
+    private func popUpAndGoTo(link: Link) {
+        let alert = UIAlertController(title: "Redirect to External Link", message: "You will be redirected to \(link.name), Are you sure to continue?", preferredStyle: UIAlertControllerStyle.alert)
+        alert.addAction(UIAlertAction(title: "No", style: .default, handler: nil))
+        alert.addAction(UIAlertAction(title: "Yes", style: .default, handler: { (_) in
+            UIApplication.shared.open(link.url, options: [:])
+        }))
+        self.present(alert, animated: true, completion: nil)
+    }
+    
+    
 }
