2ed789f83b03da96b200e75c183dff8f4d10c59d
DominicHolmes <dominic.f.holmes@gmail.com>
2018-07-02 22:31:32 -0500

    almost completed implementation of DiningMenuAPI - just needs an Enum for DiningAttributeTypes (eg: Vegetarian, Seafood, etc.)

--- PennMobile/Dining/Model/DiningMenu.swift
+++ PennMobile/Dining/Model/DiningMenu.swift
@@ -9,49 +9,77 @@
 import Foundation
 
 struct DiningMenuDocument: Codable {
-    let Document: DiningMenu
+    let document: DiningMenu
+    
+    enum CodingKeys : String, CodingKey {
+        case document = "Document"
+    }
 }
 
 struct DiningMenu: Codable {
     let location: String
-    let menudate: String
-    let tblMenu: TableMenu
+    let date: String
+    let menu: DiningDayMenu
+    
+    enum CodingKeys : String, CodingKey {
+        case location = "location"
+        case date = "menudate"
+        case menu = "tblMenu"
+    }
 }
 
-struct TableMenu: Codable {
-    let tblDayPart: [TableDayPart]
+struct DiningDayMenu: Codable {
+    let meals: [DiningMeal]
+    
+    enum CodingKeys : String, CodingKey {
+        case meals = "tblDayPart"
+    }
 }
 
-struct TableDayPart: Codable {
-    let tblStation: [TableStation]
-    let txtDayPartDescription: String
+struct DiningMeal: Codable {
+    let stations: [DiningStation]
+    let description: String
+    
+    enum CodingKeys : String, CodingKey {
+        case stations = "tblStation"
+        case description = "txtDayPartDescription"
+    }
 }
 
-struct TableStation: Codable {
-    let tblItem: [TableItem]
-    let txtStationDescription: String
+struct DiningStation: Codable {
+    let menuItem: [MenuItem]
+    let description: String
+    
+    enum CodingKeys : String, CodingKey {
+        case menuItem = "tblItem"
+        case description = "txtStationDescription"
+    }
 }
 
-struct TableItem: Codable {
-    let tblAttributes: TextAttribute?
-    /*tblFarmToFork: "",
-     tblSide: "",
-     txtDescription: "local cage free eggs lightly seasoned with kosher salt and black pepper, scrambled to perfection&lt;br /&gt;common market philadelphia",
-     txtNutritionInfo: "",
-     txtPrice: "",
-     txtTitle: "scrambled eggs"*/
+struct MenuItem: Codable {
+    let attributes: MenuItemAttributes?
+    let title: String
+    let description: String
+    
+    enum CodingKeys : String, CodingKey {
+        case attributes = "tblAttributes"
+        case title = "txtTitle"
+        case description = "txtDescription"
+    }
+    
     init(from decoder: Decoder) throws {
         let container = try decoder.container(keyedBy: CodingKeys.self)
+        self.title = try container.decode(String.self, forKey: .title)
+        self.description = try container.decode(String.self, forKey: .description)
         do {
-            self.tblAttributes = try container.decodeIfPresent(TextAttribute.self,
-                                                               forKey: .tblAttributes)
+            self.attributes = try container.decodeIfPresent(MenuItemAttributes.self, forKey: .attributes)
         } catch {
-            self.tblAttributes = nil
+            self.attributes = nil
         }
     }
 }
 
-struct TextAttribute: Codable {
+struct MenuItemAttributes: Codable {
     let attributes: [DiningAttribute]
     enum CodingKeys : String, CodingKey {
         case attributes = "txtAttribute"
@@ -59,5 +87,9 @@ struct TextAttribute: Codable {
 }
 
 struct DiningAttribute: Codable {
-    let description: String
+    let description: DiningAttributeType
+}
+
+enum DiningAttributeType : String, Codable {
+    case Vegetarian
 }
