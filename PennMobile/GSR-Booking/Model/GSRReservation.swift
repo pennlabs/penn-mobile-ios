dc481389126a1666c6859aadbee5321c9e52eefd
Josh <jsdoman97@gmail.com>
2019-02-14 23:07:42 -0500

    networking

---
+++ PennMobile/GSR-Booking/Model/GSRReservation.swift
@@ -0,0 +1,17 @@
+//
+//  GSRReservation.swift
+//  PennMobile
+//
+//  Created by Josh Doman on 2/14/19.
+//  Copyright © 2019 PennLabs. All rights reserved.
+//
+
+import Foundation
+
+struct GSRReservation {
+    let id: Int
+    let date: String
+    let location: String
+    let startTime: String
+    let endTime: String
+}
