1c6b8dac3c35d10a76ae6275640c16a3826eef92
Daniel Salib <dsalib@sas.upenn.edu>
2019-02-21 11:24:45 -0500

    changed frontera to short name everywhere

--- PennMobile/Dining/Cells/DiningCell.swift
+++ PennMobile/Dining/Cells/DiningCell.swift
@@ -49,7 +49,8 @@ extension DiningCell {
     fileprivate func setupCell(with venue: DiningVenue) {
         venueImageView.image = UIImage(named: venue.name.rawValue.folding(options: .diacriticInsensitive, locale: .current))
         
-        if isHomepage {
+        //Fix to get rid of Frontera's super long name
+        if isHomepage || venue.name == .frontera {
             titleLabel.text = DiningVenueName.getShortVenueName(for: venue.name)
         } else {
             titleLabel.text = venue.name.rawValue
