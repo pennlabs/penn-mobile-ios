8ae0148a2ce5a444ce7b11ab97129855efe4d10b
LucyYYW <yueweilucy@126.com>
2019-10-18 17:19:08 -0400

    add some ui variables

---
+++ PennMobile/GSR-Booking/Controllers/GSRGroupNewIntialController.swift
@@ -0,0 +1,45 @@
+//
+//  GSRGroupNewIntialController.swift
+//  PennMobile
+//
+//  Created by Lucy Yuewei Yuan on 10/18/19.
+//  Copyright © 2019 PennLabs. All rights reserved.
+//
+
+import UIKit
+
+class GSRGroupNewIntialController: UIViewController {
+
+    fileprivate var closeButton: UIButton!
+    fileprivate var nameField: UITextView!
+    fileprivate var groupForLabel: UILabel!
+    fileprivate var barView: UIView! //need to figure out later
+    fileprivate var colorLabel: UILabel!
+    fileprivate var colorPanel: UIView!
+    fileprivate var createButton: UIButton!
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    
+    
+    
+    
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
+
