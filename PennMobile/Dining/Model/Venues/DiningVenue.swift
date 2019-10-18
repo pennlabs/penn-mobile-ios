6be353670564fc45386c729ec87e8f733336b409
dominic <hello@dom.voyage>
2019-10-18 18:18:08 -0400

    Made some progress in dining api rewrite

--- PennMobile/Dining/Model/Venues/DiningVenue.swift
+++ PennMobile/Dining/Model/Venues/DiningVenue.swift
@@ -18,7 +18,117 @@ enum DiningVenueType: String, Codable {
     }
 }
 
-class DiningVenue: NSObject {
+struct DiningVenue: Codable {
+    let dailyMenuURL: URL
+    let dateHours: [DateMeals]?
+    let facilityURL: URL
+    let id: Int
+    let name: String
+    let venueType: DiningVenueType
+    let weeklyMenuURL: String
+    
+    struct DateMeals: Codable {
+        let date: String
+        let meal: [CodableOpenClose]
+    }
+}
+
+struct CodableOpenClose: Codable {
+    let open: String
+    let close: String
+    let meal: String
+    
+    enum CodingKeys: String, CodingKey {
+        case open = "open"
+        case close = "close"
+        case meal = "type"
+    }
+}
+
+// CodableOpenClose is used to decode the JSON, and then it is mapped to OpenClose
+// This is required because OpenClose needs to know the date string from one level higher
+
+struct OpenClose: Equatable {
+    let open: Date
+    let close: Date
+    let meal: String
+
+//    static let completeformatter: DateFormatter = {
+//        let df = DateFormatter()
+//        df.dateFormat = "yyyy-MM-dd:HH:mm:ss"
+//        df.timeZone = TimeZone(abbreviation: "EST")
+//        return df
+//    }()
+    
+    var description: String {
+        return open.description + " - " + close.description
+    }
+    
+    func overlaps(with oc: OpenClose) -> Bool {
+        return (oc.open >= self.open && oc.open < self.close) || (self.open >= oc.open && self.open < oc.close)
+    }
+    
+    func withoutMinutes() -> OpenClose {
+        let newOpen = open.roundedDownToHour
+        let newClose = close.roundedDownToHour
+        return OpenClose(open: newOpen, close: newClose, meal: meal)
+    }
+}
+
+// CodableOpenClose is converted to OpenClose in this function
+fileprivate func processDecodedHours(hours: DiningVenueForWeek, for venue: DiningVenueName) {
+    
+    let formatter = DateFormatter()
+    formatter.dateFormat = "yyyy-MM-dd:HH:mm:ss"
+    formatter.timeZone = TimeZone(abbreviation: "EST")
+    
+    if let dateHours = hours.dateHours {
+        for eachDayIndex in dateHours.indices {
+            let eachDay = dateHours[eachDayIndex]
+            
+            var hoursForDay = [OpenClose]()
+            
+            let dateString = eachDay.date
+            var closedFlag = false
+            var closedTime: OpenClose?
+            
+            for eachMeal in eachDay.meal {
+                let openString = dateString + ":" + eachMeal.open
+                let closeString = dateString + ":" + eachMeal.close
+                
+                guard let openDate = formatter.date(from: openString)?.adjustedFor11_59,
+                    let closeDate = formatter.date(from: closeString)?.adjustedFor11_59 else { continue }
+                
+                let openClose = OpenClose(open: openDate, close: closeDate, meal: eachMeal.meal)
+                if eachMeal.meal == "Closed" {
+                    closedFlag = true
+                    closedTime = openClose
+                } else if !hoursForDay.containsOverlappingTime(with: openClose) {
+                    hoursForDay.append(openClose)
+                }
+            }
+            if let closedTime = closedTime, closedFlag {
+                hoursForDay = hoursForDay.filter { !$0.overlaps(with: closedTime) }
+            }
+            
+            self.load(hours: hoursForDay, on: eachDay.date, for: venue)
+        }
+    }
+}
+
+fileprivate func getIdMapping(jsonArray: [JSON]) -> [Int: String] {
+    var mapping = [Int: String]()
+    for json in jsonArray {
+        let name = json["name"].stringValue
+        let id = json["id"].intValue
+        mapping[id] = name
+    }
+    return mapping
+}
+
+
+
+/*class DiningVenue: NSObject {
     
     static let diningNames: [DiningVenueName] = [.commons, .mcclelland, .lauder, .hill, .english, .falk]
     static let retailNames: [DiningVenueName] = [.houston, .gourmetGrocer, .joes, .starbucks, .pret, .mbaCafe]
@@ -69,7 +179,7 @@ class DiningVenue: NSObject {
         
         return names.map { DiningVenue(venue: $0) }
     }
-}
+}*/
 
 // MARK: - Caching
 extension DiningVenue {
@@ -167,5 +277,87 @@ extension DiningVenue: BuildingMapDisplayable {
     
     func getAnnotation() -> MKAnnotation {
         return PennCoordinate.shared.getAnnotation(for: self.name)
+    }
+}
+
+// MARK: - Array Extension
+extension Array where Element == OpenClose {
+    func containsOverlappingTime(with oc: OpenClose) -> Bool {
+        for e in self {
+            if e.overlaps(with: oc) { return true }
+        }
+        return false
+    }
+    
+    mutating func removeAllMinutes() {
+        self = self.map({ (oc) -> OpenClose in
+            oc.withoutMinutes()
+        })
+    }
+    
+    var isOpen: Bool {
+        let now = Date()
+        for open_close in self {
+            if open_close.open < now && open_close.close > now {
+                return true
+            }
+        }
+        return false
+    }
+    
+    var nextOpen: OpenClose? {
+        let now = Date()
+        for index in self.indices {
+            let open_close = self[index]
+            
+            // If the call is currently open, return the current timeslot
+            if open_close.open < now && open_close.close > now { return open_close }
+            
+            // If the hall is closed but about to open again, return the next timeslot
+            if index + 1 < self.count {
+                if self[index].close < now && self[index + 1].open > now { return self[index + 1] }
+            }
+        }
+        return nil
+    }
+    
+    var strFormat: String {
+        let formatter = DateFormatter()
+        formatter.locale = Locale(identifier: "en_US_POSIX")
+        formatter.timeZone = TimeZone(abbreviation: "EST")
+        formatter.dateFormat = "h:mma"
+        formatter.amSymbol = "a"
+        formatter.pmSymbol = "p"
+        
+        var firstOpenClose = true
+        var timesString = ""
+        
+        for open_close in self {
+            if open_close.open.minutes == 0 {
+                formatter.dateFormat = self.count > 1 ? "h" : "ha"
+            } else {
+                formatter.dateFormat = self.count > 1 ? "h:mm" : "h:mma"
+            }
+            let open = formatter.string(from: open_close.open)
+            
+            if open_close.close.minutes == 0 {
+                formatter.dateFormat = self.count > 1 ? "h" : "ha"
+            } else {
+                formatter.dateFormat = self.count > 1 ? "h:mm" : "h:mma"
+            }
+            let close = formatter.string(from: open_close.close)
+            
+            if firstOpenClose {
+                firstOpenClose = false
+            } else {
+                timesString += "  |  "
+            }
+            timesString += "\(open) - \(close)"
+        }
+        
+        if self.isEmpty {
+            timesString = ""
+        }
+        return timesString
     }
 }
