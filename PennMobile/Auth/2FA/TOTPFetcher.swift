c7edf7a4d1cbfc90ce79ae1cb6044d892e951a82
Henrique <henrique.lorente@gmail.com>
2019-10-18 17:34:32 -0400

    fixed comment

--- PennMobile/Auth/2FA/TOTPFetcher.swift
+++ PennMobile/Auth/2FA/TOTPFetcher.swift
@@ -24,7 +24,6 @@ class TOTPFetcher: NSObject {
                 let secureStore =
                     SecureStore(secureStoreQueryable: genericPwdQueryable)
                 try? secureStore.setValue(secret, for: "TOTPSecret")
-//                TOTPFetcher.instance.fetchAndSaveTOTPSecret()
             }
             completion?(secret)
         }
