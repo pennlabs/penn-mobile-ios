1170fc8b85ab94fc8ca5df76983a89c752515621
dominic <hello@dom.voyage>
2019-12-31 12:50:10 -0800

    Added privacy permission delegate observableobject

---
+++ PennMobile/About/Privacy/PrivacyPermissionDelegate.swift
@@ -0,0 +1,25 @@
+//
+//  PrivacyPermissionDelegate.swift
+//  PennMobile
+//
+//  Created by Dominic Holmes on 12/31/19.
+//  Copyright © 2019 PennLabs. All rights reserved.
+//
+
+import SwiftUI
+import Combine
+
+@available(iOS 13, *)
+class PrivacyPermissionDelegate: ObservableObject {
+    var objectWillChange = PassthroughSubject<PrivacyPermissionDelegate, Never>()
+    var objectDidChange = PassthroughSubject<PrivacyPermissionDelegate, Never>()
+    
+    var contact: String? {
+        willSet {
+            objectWillChange.send(self)
+        }
+        didSet {
+            objectDidChange.send(self)
+        }
+    }
+}
