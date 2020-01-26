b24ca3716e741599e55c3fb149910c602d131f4d
LucyYYW <yueweilucy@126.com>
2020-01-26 13:06:14 -0500

    show selected users when not searching; show checkmarks for selected users

---
+++ PennMobile/GSR-Booking/Views/GSRGroups/GroupInviteUserCell.swift
@@ -0,0 +1,21 @@
+//
+//  GroupInviteUserCell.swift
+//  PennMobile
+//
+//  Created by Lucy Yuewei Yuan on 1/26/20.
+//  Copyright © 2020 PennLabs. All rights reserved.
+//
+
+import SwiftUI
+
+struct GroupInviteUserCell: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct GroupInviteUserCell_Previews: PreviewProvider {
+    static var previews: some View {
+        GroupInviteUserCell()
+    }
+}
