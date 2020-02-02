28c28e241a70d3d364df78d6bae50e400864db96
LucyYYW <yueweilucy@126.com>
2020-02-02 11:59:32 -0500

    adding two files for pending invites under home/cells

---
+++ PennMobile/Home/Cells/PendingInvites/HomePendingInvitesCell.swift
@@ -0,0 +1,9 @@
+//
+//  HomePendingInvitesCell.swift
+//  PennMobile
+//
+//  Created by Lucy Yuewei Yuan on 2/2/20.
+//  Copyright © 2020 PennLabs. All rights reserved.
+//
+
+import Foundation
---
+++ PennMobile/Home/Cells/PendingInvites/HomePendingInvitesCellItem.swift
@@ -0,0 +1,9 @@
+//
+//  HomePendingInvitesCellItem.swift
+//  PennMobile
+//
+//  Created by Lucy Yuewei Yuan on 2/2/20.
+//  Copyright © 2020 PennLabs. All rights reserved.
+//
+
+import Foundation
