987878edda2d0b41bde0ba3c5a11fe87815bde95
Henrique Lorente <henrique.lorente@gmail.com>
2020-02-20 14:29:17 -0500

    Fixed extensions to UIColor in Colors.swift (#312)
    * made all colors let variables and fixed the lighter colors
    
    * Reverting podfile
    
    * Update Colors.swift
    
    Co-authored-by: dominic <hello@dominic.land>

--- PennMobile/General/Colors.swift
+++ PennMobile/General/Colors.swift
@@ -27,53 +27,53 @@ extension UIColor {
     static let labelQuaternary = UIColor(named: "labelQuaternary")!
     
     // MARK: - Primary Palette
-    static var baseDarkBlue = UIColor(named: "baseDarkBlue")!
+    static let baseDarkBlue = UIColor(named: "baseDarkBlue")!
     static let baseLabsBlue = UIColor(named: "baseLabsBlue")!
     
     // MARK: - Neutral Palette
-    static var grey1 = UIColor(named: "grey1")!
-    static var grey2 = UIColor(named: "grey2")!
-    static var grey3 = UIColor(named: "grey3")!
-    static var grey4 = UIColor(named: "grey4")!
-    static var grey5 = UIColor(named: "grey5")!
-    static var grey6 = UIColor(named: "grey6")!
+    static let grey1 = UIColor(named: "grey1")!
+    static let grey2 = UIColor(named: "grey2")!
+    static let grey3 = UIColor(named: "grey3")!
+    static let grey4 = UIColor(named: "grey4")!
+    static let grey5 = UIColor(named: "grey5")!
+    static let grey6 = UIColor(named: "grey6")!
     
     // MARK: - Secondary Palette
-    static var baseBlue = UIColor(named: "baseBlue")!
-    static var baseGreen = UIColor(named: "baseGreen")!
-    static var baseOrange = UIColor(named: "baseOrange")!
-    static var basePurple = UIColor(named: "basePurple")!
-    static var baseRed = UIColor(named: "baseRed")!
-    static var baseYellow = UIColor(named: "baseYellow")!
+    static let baseBlue = UIColor(named: "baseBlue")!
+    static let baseGreen = UIColor(named: "baseGreen")!
+    static let baseOrange = UIColor(named: "baseOrange")!
+    static let basePurple = UIColor(named: "basePurple")!
+    static let baseRed = UIColor(named: "baseRed")!
+    static let baseYellow = UIColor(named: "baseYellow")!
     
     // MARK: - Extended Palette
-    static var blueLight = UIColor(named: "blueLighter")!
-    static var blueLighter = UIColor(named: "blueLighter")!
-    static var blueDark = UIColor(named: "blueDark")!
-    static var blueDarker = UIColor(named: "blueDarker")!
+    static let blueLight = UIColor(named: "blueLight")!
+    static let blueLighter = UIColor(named: "blueLighter")!
+    static let blueDark = UIColor(named: "blueDark")!
+    static let blueDarker = UIColor(named: "blueDarker")!
     
-    static var greenLight = UIColor(named: "greenLighter")!
-    static var greenLighter = UIColor(named: "greenLighter")!
-    static var greenDark = UIColor(named: "greenDark")!
-    static var greenDarker = UIColor(named: "greenDarker")!
+    static let greenLight = UIColor(named: "greenLight")!
+    static let greenLighter = UIColor(named: "greenLighter")!
+    static let greenDark = UIColor(named: "greenDark")!
+    static let greenDarker = UIColor(named: "greenDarker")!
 
-    static var orangeLight = UIColor(named: "orangeLighter")!
-    static var orangeLighter = UIColor(named: "orangeLighter")!
-    static var orangeDark = UIColor(named: "orangeDark")!
-    static var orangeDarker = UIColor(named: "orangeDarker")!
+    static let orangeLight = UIColor(named: "orangeLight")!
+    static let orangeLighter = UIColor(named: "orangeLighter")!
+    static let orangeDark = UIColor(named: "orangeDark")!
+    static let orangeDarker = UIColor(named: "orangeDarker")!
 
-    static var purpleLight = UIColor(named: "purpleLighter")!
-    static var purpleLighter = UIColor(named: "purpleLighter")!
-    static var purpleDark = UIColor(named: "purpleDark")!
-    static var purpleDarker = UIColor(named: "purpleDarker")!
+    static let purpleLight = UIColor(named: "purpleLight")!
+    static let purpleLighter = UIColor(named: "purpleLighter")!
+    static let purpleDark = UIColor(named: "purpleDark")!
+    static let purpleDarker = UIColor(named: "purpleDarker")!
     
-    static var redLight = UIColor(named: "redLight")!
-    static var redLighter = UIColor(named: "redLighter")!
-    static var redDark = UIColor(named: "redDark")!
-    static var redDarker = UIColor(named: "redDarker")!
+    static let redLight = UIColor(named: "redLight")!
+    static let redLighter = UIColor(named: "redLighter")!
+    static let redDark = UIColor(named: "redDark")!
+    static let redDarker = UIColor(named: "redDarker")!
 
-    static var yellowLight = UIColor(named: "yellowLighter")!
-    static var yellowLighter = UIColor(named: "yellowLighter")!
-    static var yellowDark = UIColor(named: "yellowDark")!
-    static var yellowDarker = UIColor(named: "yellowDarker")!
+    static let yellowLight = UIColor(named: "yellowLight")!
+    static let yellowLighter = UIColor(named: "yellowLighter")!
+    static let yellowDark = UIColor(named: "yellowDark")!
+    static let yellowDarker = UIColor(named: "yellowDarker")!
 }
