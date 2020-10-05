d9e6716901c12ebeb56ddc0cdae2fd9480e59dcb
Daniel Duan <daniel4duan@gmail.com>
2020-10-04 18:43:28 -0700

    added daniel duan to about page

--- PennMobile/About/Controllers/AboutViewController.swift
+++ PennMobile/About/Controllers/AboutViewController.swift
@@ -73,13 +73,14 @@ class AboutViewController : UIViewController, UICollectionViewDelegateFlowLayout
         let jongmin = Member(firstName: "Jong Min", lastName: "Choi", image: "jongmin.jpeg")
         let adam = Member(firstName: "Adam", lastName: "Strike", image: "adam.jpeg")
         let justin = Member(firstName: "Justin", lastName: "Lieb", image: "justin.jpeg")
+        let daniel = Member(firstName: "Daniel", lastName: "Duan", image: "justin.jpeg")
 
         var currentMembers = [Member]()
         var pastMembers = [Member]()
         
         //fill the arrays with members, and sort alphabetically
         pastMembers += [marta, grace, ben, tiff, zhilei, laura, adel, yagil, josh, dom, carin, salib]
-        currentMembers += [rehaan, liz, henrique, lucy, matthew, hassan, jongmin, adam, justin]
+        currentMembers += [rehaan, liz, henrique, lucy, matthew, hassan, jongmin, adam, justin, daniel]
 
         pastMembers.sort(by: {$0 < $1})
         currentMembers.sort(by: {$0 < $1})
