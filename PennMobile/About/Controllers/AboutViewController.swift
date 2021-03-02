35f1d1ac5607cab49a3caf4fa33113bcc3d25a70
Benjamin Kaufman <bsk42@wharton.upenn.edu>
2021-03-02 12:52:25 -0500

    this is my first commit!!!

--- PennMobile/About/Controllers/AboutViewController.swift
+++ PennMobile/About/Controllers/AboutViewController.swift
@@ -75,13 +75,14 @@ class AboutViewController : UIViewController, UICollectionViewDelegateFlowLayout
         let justin = Member(firstName: "Justin", lastName: "Lieb", image: "justin.jpeg")
         let daniel = Member(firstName: "Daniel", lastName: "Duan", image: "daniel.jpg")
         let raunaq = Member(firstName: "Raunaq", lastName: "Singh", image: "raunaq.jpeg")
+        let benK = Member(firstName: "Benjamin", lastName: "Kaufman", image: "benK.jpeg")
 
         var currentMembers = [Member]()
         var pastMembers = [Member]()
         
         //fill the arrays with members, and sort alphabetically
         pastMembers += [marta, grace, ben, tiff, zhilei, laura, adel, yagil, josh, dom, carin, salib, liz]
-        currentMembers += [rehaan, henrique, lucy, matthew, hassan, jongmin, adam, justin, raunaq, daniel]
+        currentMembers += [rehaan, henrique, lucy, matthew, hassan, jongmin, adam, justin, raunaq, daniel, benK]
 
         pastMembers.sort(by: {$0 < $1})
         currentMembers.sort(by: {$0 < $1})
