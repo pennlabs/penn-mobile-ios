ed92451396af41f20c18bced37ac4ff41ed709d9
Jong Min Choi <01jongminchoi@gmail.com>
2022-01-25 09:55:11 -0500

    migrated post to new api

--- PennMobile/Home/Cells/Post/Post.swift
+++ PennMobile/Home/Cells/Post/Post.swift
@@ -7,52 +7,13 @@
 //
 
 import Foundation
-import SwiftyJSON
 
-class Post {
-    let source: String?
+struct Post: Codable {
+    let id: Int
     let title: String?
     let subtitle: String?
-    let timeLabel: String?
-    let imageUrl: String
     let postUrl: String?
-    let id: Int
-    let isTest: Bool
-    
-    init(source: String?, title: String?, subtitle: String?, timeLabel: String?, imageUrl: String, postUrl: String?, id: Int, isTest: Bool) {
-        self.source = source
-        self.title = title
-        self.subtitle = subtitle
-        self.timeLabel = timeLabel
-        self.imageUrl = imageUrl
-        self.postUrl = postUrl
-        self.id = id
-        self.isTest = isTest
-    }
-}
-
-// MARK: - JSON Parsing
-extension Post {
-    convenience init(json: JSON) throws {
-        guard let imageUrl = json["image_url"].string, let id = json["post_id"].int, let isTest = json["test"].bool else {
-            // All posts must have at least an image, an id, and a test flag
-            throw NetworkingError.jsonError
-        }
-        
-        let source = json["source"].string
-        let title = json["title"].string
-        let subtitle = json["subtitle"].string
-        let postUrl = json["post_url"].string
-        let timeLabel = json["time_label"].string
-        
-        if (source == nil && timeLabel != nil) || (title == nil && subtitle == nil && source != nil) || (title == nil && subtitle != nil) {
-            // Rules:
-            //  (1) A time label cannot exist without a source label
-            //  (2) An image cannot be accompanied with only a source label
-            //  (3) A subtitle cannot exist without a title
-            throw NetworkingError.jsonError
-        }
-        
-        self.init(source: source, title: title, subtitle: subtitle, timeLabel: timeLabel, imageUrl: imageUrl, postUrl: postUrl, id: id, isTest: isTest)
-    }
+    let imageUrl: String
+    let createdDate: String?
+    let source: String?
 }
