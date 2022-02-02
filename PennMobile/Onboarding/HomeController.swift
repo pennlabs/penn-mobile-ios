81b7df1b295e022c44f8e474ce74251ed0874979
Jong Min Choi <01jongminchoi@gmail.com>
2022-02-02 16:05:49 -0500

    fixed white space v2

--- PennMobile/Onboarding/HomeController.swift
+++ PennMobile/Onboarding/HomeController.swift
@@ -9,26 +9,23 @@
 import UIKit
 
 class HomeController: UIViewController {
-    
     override func viewDidLoad() {
         super.viewDidLoad()
-        
+
         navigationItem.title = "We're logged in"
-        
+
         navigationItem.leftBarButtonItem = UIBarButtonItem(title: "Sign Out", style: .plain, target: self, action: #selector(handleSignOut))
-        
+
         let imageView = UIImageView(image: UIImage(named: "home"))
-        
+
         //64 pixels: 20 for status bar, 44 for navigation bar
         view.addSubview(imageView)
         _ = imageView.anchor(view.topAnchor, left: view.leftAnchor, bottom: view.bottomAnchor, right: view.rightAnchor, topConstant: 64, leftConstant: 0, bottomConstant: 0, rightConstant: 0, widthConstant: 0, heightConstant: 0)
     }
-    
+
     func handleSignOut() {
         UserDefaults.standard.(value: false)
-        
         let loginController = LoginController()
         present(loginController, animated: true, completion: nil)
     }
-    
 }
