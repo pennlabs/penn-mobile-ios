f4a034a58f07ca1ceb008a0e860ba1bf197c8bec
Jong Min Choi <01jongminchoi@gmail.com>
2022-02-06 12:38:47 -0500

    cleanup splashVC

--- PennMobile/Setup + Navigation/SplashViewController.swift
+++ PennMobile/Setup + Navigation/SplashViewController.swift
@@ -15,29 +15,5 @@ class SplashViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = .uiBackground
-        
-        if #available(iOS 15.0, *) {
-            Task {
-                do {
-                    let (data, _) = try await URLSession.shared.data(from: URL(string: "https://google.com")!)
-                    print(String(data: data, encoding: .utf8))
-                } catch {
-                    print("error?")
-                    
-                }
-                switchToView()
-            }
-        } else {
-            switchToView()
-        }
-    }
-    
-    private func switchToView() {
-        let loggedIn = UserDefaults.standard.getAccountID() != nil
-        if loggedIn {
-            AppDelegate.shared.rootViewController.showMainScreen()
-        } else {
-            AppDelegate.shared.rootViewController.showLoginScreen()
-        }
     }
 }
