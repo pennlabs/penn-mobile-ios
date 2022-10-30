a037b42c2b797327d4ed29fd1f854de82ca152ec
Kunli Zhang <kunlizhang7@gmail.com>
2022-10-30 13:34:16 -0400

    Remove print statement

--- PennMobile/Auth/OAuth2NetworkManager.swift
+++ PennMobile/Auth/OAuth2NetworkManager.swift
@@ -81,7 +81,6 @@ extension OAuth2NetworkManager {
         // dev token that expires in a year
         // use until auth is back up
         if let accessToken = self.currentAccessToken, Date() < accessToken.expiration {
-            print(accessToken)
             callback(accessToken)
         } else {
             self.currentAccessToken = nil
