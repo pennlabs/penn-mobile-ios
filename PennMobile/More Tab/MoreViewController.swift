591fab9cbd9eb717d8423663d87cbfe6ca5887f0
nitinseshadri <61258126+nitinseshadri@users.noreply.github.com>
2023-01-06 23:18:45 -0500

    Replace PennInTouch link in More tab with link to Path@Penn

--- PennMobile/More Tab/MoreViewController.swift
+++ PennMobile/More Tab/MoreViewController.swift
@@ -78,7 +78,7 @@ class MoreViewController: GenericTableViewController, ShowsAlert {
         PennLink(title: "Penn Homepage", url: "https://upenn.edu"),
         PennLink(title: "CampusExpress", url: "https://prod.campusexpress.upenn.edu"),
         PennLink(title: "Canvas", url: "https://canvas.upenn.edu"),
-        PennLink(title: "PennInTouch", url: "https://pennintouch.apps.upenn.edu"),
+        PennLink(title: "Path@Penn", url: "https://path.at.upenn.edu"),
         PennLink(title: "PennPortal", url: "https://portal.apps.upenn.edu/penn_portal"),
         PennLink(title: "Share Your Feedback", url: "https://airtable.com/shrS98E3rj5Nw1wy6")]
 
