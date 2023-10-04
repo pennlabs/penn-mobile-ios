7a28e9a306a06b729dfc91feb11588be5840535b
JHawk0224 <jhawk111@icloud.com>
2023-10-04 12:50:49 -0400

    Add voted detail

--- PennMobile/Polls/PollsView.swift
+++ PennMobile/Polls/PollsView.swift
@@ -15,8 +15,7 @@ struct PollsView: View {
         ScrollView {
             VStack {
                 ForEach(polls, id: \.self.id) { poll in
-                    HomePollsCellWrapper(pollQuestion: poll.poll)
-                        .frame(height: HomePollsCell.getPollHeight(for: poll.poll))
+                    PollWrapper(pollQuestion: poll.poll, chosenId: poll.pollOptions[0].id)
                 }
             }
         }
@@ -32,52 +31,28 @@ struct PollsView: View {
     }
 }
 
-struct HomePollsCellWrapper: UIViewRepresentable {
+struct PollWrapper: UIViewRepresentable {
     let pollQuestion: PollQuestion
+    let chosenId: Int?
+    var answeredPollQuestion: PollQuestion {
+        var question = pollQuestion
+        question.optionChosenId = chosenId
+        return question
+    }
 
     func makeUIView(context: Context) -> HomePollsCell {
         let cell = HomePollsCell()
-        cell.pollQuestion = pollQuestion
+        cell.pollQuestion = answeredPollQuestion
         cell.isUserInteractionEnabled = false
-        // do something with vote data
-//        for cell in cell.tableView.visibleCells as! [PollOptionCell] {
-//            cell.answered = true
-//        }
         return cell
+    }
+    
+    @available(iOS 16.0, *)
+    func sizeThatFits(_ proposal: ProposedViewSize, uiView: HomePollsCell, context: Context) -> CGSize? {
+        return CGSize(width: proposal.width ?? 0, height: HomePollsCell.getPollHeight(for: answeredPollQuestion))
     }
 
     func updateUIView(_ uiView: HomePollsCell, context: Context) {
-        uiView.pollQuestion = pollQuestion
+        uiView.pollQuestion = answeredPollQuestion
     }
 }
-
-//
-//func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-//    let cell = tableView.dequeueReusableCell(withIdentifier: PollOptionCell.identifier, for: indexPath) as! PollOptionCell
-//    let pollOption = pollQuestion.options[indexPath.row]
-//
-//    cell.totalResponses = pollQuestion.totalVoteCount
-//    cell.answered = (pollQuestion.optionChosenId != nil)
-//    cell.chosen = pollQuestion.optionChosenId == pollOption.id
-//
-//    cell.pollOption = pollOption
-//
-//    return cell
-//}
-//
-//DispatchQueue.main.async {
-//    self.pollQuestion.options[indexPath.row].voteCount += 1
-//    // Change selected cell to chosen
-//    let chosenCell = (tableView.cellForRow(at: indexPath) as! PollOptionCell)
-//    chosenCell.pollOption.voteCount += 1
-//    chosenCell.chosen = true
-//
-//    // Update cells to reflect question answered
-//    for cell in tableView.visibleCells as! [PollOptionCell] {
-//        cell.totalResponses += 1
-//        cell.answered = true
-//    }
-//
-//    // Update model
-//    self.pollQuestion.optionChosenId = self.pollQuestion.options[indexPath.row].id
-//}
