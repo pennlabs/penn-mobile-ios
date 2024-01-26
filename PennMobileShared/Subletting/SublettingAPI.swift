6a3bb4e2ab82e755d11f834a165572a891f590e3
Anthony Li <me@anli.dev>
2024-01-26 17:54:05 -0500

    Add draft version of subletting endpoints

---
+++ PennMobileShared/Subletting/SublettingAPI.swift
@@ -0,0 +1,115 @@
+//
+//  SublettingAPI.swift
+//  PennMobile
+//
+//  Created by Jordan H on 1/26/24.
+//  Copyright © 2024 PennLabs. All rights reserved.
+//
+
+import Foundation
+
+public class SublettingAPI {
+
+    public static let instance = SublettingAPI()
+    public let sublettingUrl = "https://pennmobile.org/api/sublet/properties/"
+
+    public func createSublet(subletData: SubletData) async throws -> Sublet {
+        guard let url = URL(string: sublettingUrl) else {
+            throw NetworkingError.serverError
+        }
+        
+        var request = URLRequest(url: url)
+        request.httpMethod = "POST"
+        request.addValue("application/json", forHTTPHeaderField: "Content-Type")
+        
+        let encoder = JSONEncoder()
+        encoder.dateEncodingStrategy = .iso8601
+        encoder.keyEncodingStrategy = .convertToSnakeCase
+        do {
+            let jsonData = try encoder.encode(subletData)
+            request.httpBody = jsonData
+        } catch {
+            throw NetworkingError.parsingError
+        }
+        
+        let (data, _) = try await URLSession.shared.data(for: request)
+        
+        let decoder = JSONDecoder()
+        let dateFormatter = DateFormatter()
+        dateFormatter.dateFormat = "yyyy-MM-dd'T'HH:mm:ssZ"
+        decoder.dateDecodingStrategy = .formatted(dateFormatter)
+        decoder.keyDecodingStrategy = .convertFromSnakeCase
+        
+        let sublet = try decoder.decode(Sublet.self, from: data)
+        return sublet
+    }
+
+    public func destroySublet(id: Int) async throws {
+        let urlString = "\(sublettingUrl)\(id)/"
+        guard let url = URL(string: urlString) else {
+            throw NetworkingError.serverError
+        }
+        
+        var request = URLRequest(url: url)
+        request.httpMethod = "DELETE"
+        
+        let (_, response) = try await URLSession.shared.data(for: request)
+        
+        guard let httpResponse = response as? HTTPURLResponse, httpResponse.statusCode == 200 else {
+            throw NetworkingError.serverError
+        }
+    }
+
+    public func patchSublet(sublet: Sublet) async throws {
+        let urlString = "\(sublettingUrl)\(sublet.id)/"
+        guard let url = URL(string: urlString) else {
+            throw NetworkingError.serverError
+        }
+        
+        var request = URLRequest(url: url)
+        request.httpMethod = "PATCH"
+        request.addValue("application/json", forHTTPHeaderField: "Content-Type")
+        
+        let encoder = JSONEncoder()
+        encoder.dateEncodingStrategy = .iso8601
+        encoder.keyEncodingStrategy = .convertToSnakeCase
+        do {
+            let jsonData = try encoder.encode(sublet)
+            request.httpBody = jsonData
+        } catch {
+            throw NetworkingError.parsingError
+        }
+        
+        let (_, response) = try await URLSession.shared.data(for: request)
+        
+        guard let httpResponse = response as? HTTPURLResponse, httpResponse.statusCode == 200 else {
+            throw NetworkingError.serverError
+        }
+    }
+
+    public func getSublets(queryParameters: [String: String]? = nil) async throws -> [Sublet] {
+        var urlComponents = URLComponents(string: sublettingUrl)!
+        
+        if let queryParameters = queryParameters, !queryParameters.isEmpty {
+            urlComponents.queryItems = queryParameters.map { URLQueryItem(name: $0.key, value: $0.value) }
+        }
+        
+        guard let url = urlComponents.url else {
+            throw NetworkingError.serverError
+        }
+        
+        var request = URLRequest(url: url)
+        request.httpMethod = "GET"
+        
+        let (data, _) = try await URLSession.shared.data(for: request)
+        
+        let decoder = JSONDecoder()
+        let dateFormatter = DateFormatter()
+        dateFormatter.dateFormat = "yyyy-MM-dd'T'HH:mm:ssZ"
+        decoder.dateDecodingStrategy = .formatted(dateFormatter)
+        decoder.keyDecodingStrategy = .convertFromSnakeCase
+        
+        let sublets = try decoder.decode([Sublet].self, from: data)
+        return sublets
+    }
+}
---
+++ PennMobileShared/Subletting/SublettingModels.swift
@@ -0,0 +1,87 @@
+//
+//  SublettingModels.swift
+//  PennMobile
+//
+//  Created by Anthony Li and Jordan H on 1/26/24.
+//  Copyright © 2024 PennLabs. All rights reserved.
+//
+
+import Foundation
+
+/// A sublet listing.
+/// 
+/// The listing's properties are split among two structs: `SubletData` and `Sublet`.
+/// `SubletData` contains the properties that are set by the user when creating a listing,
+/// and `Sublet` contains the remainder.
+@dynamicMemberLookup
+public struct Sublet: Identifiable, Decodable {
+    public let id: Int
+    public let data: SubletData
+    public let createdAt: Date
+    public let expiresAt: Date
+    public let subletter: String
+    public let sublettees: [String]
+    public let images: [SubletImage]
+
+    public subscript<T>(dynamicMember keyPath: KeyPath<SubletData, T>) -> T {
+        data[keyPath: keyPath]
+    }
+
+    public init(id: Int, data: SubletData, createdAt: Date, expiresAt: Date, subletter: String, sublettees: [String], images: [SubletImage]) {
+        self.id = id
+        self.data = data
+        self.createdAt = createdAt
+        self.expiresAt = expiresAt
+        self.subletter = subletter
+        self.sublettees = sublettees
+        self.images = images
+    }
+
+    public init(from decoder: Decoder) throws {
+        let data = try SubletData(from: decoder)
+        
+        let container = try decoder.container(keyedBy: CodingKeys.self)
+        let id = try container.decode(Int.self, forKey: .id)
+        let createdAt = try container.decode(Date.self, forKey: .createdAt)
+        let expiresAt = try container.decode(Date.self, forKey: .expiresAt)
+        let subletter = try container.decode(String.self, forKey: .subletter)
+        let sublettees = try container.decode([String].self, forKey: .sublettees)
+        let images = try container.decode([SubletImage].self, forKey: .images)
+
+        self.init(id: id, data: data, createdAt: createdAt, expiresAt: expiresAt, subletter: subletter, sublettees: sublettees)
+    }
+
+    public enum CodingKeys: CodingKey {
+        case id
+        case data
+        case createdAt
+        case expiresAt
+        case subletter
+        case sublettees
+        case images
+    }
+}
+
+public struct SubletData: Codable {
+    public var amenities: [SubletAmenity]
+    public var title: String
+    public var address: String
+    public var beds: Int?
+    public var baths: Int?
+    public var description: String?
+    public var externalLink: String
+    public var minPrice: Int
+    public var maxPrice: Int
+    public var expiresAt: Date
+    public var startDate: Date
+    public var endDate: Date
+}
+
+public struct SubletAmenity: Codable {
+    public let name: String
+}
+
+public struct SubletImage: Decodable {
+    public let id: Int
+    public let imageUrl: String
+}
