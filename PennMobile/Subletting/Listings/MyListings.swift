8944bdfa600642ebcd408b93d870c1d142d7fdda
Nikita Mounier <nikita.mounier@gmail.com>
2024-01-28 12:53:04 -0500

    create listings foler

---
+++ PennMobile/Subletting/Listings/MyListings.swift
@@ -0,0 +1,15 @@
+import SwiftUI
+
+class ListingsViewModel: ObservableObject {
+  
+}
+
+struct MyListings: View {
+  var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    MyListings()
+}
