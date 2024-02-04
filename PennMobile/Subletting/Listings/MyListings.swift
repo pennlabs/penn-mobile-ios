6b6f0f4b643ca823814ff6659b216fd66b58aabd
George Botros <78520093+george-botros@users.noreply.github.com>
2024-02-03 19:33:43 -0500

    feat: Add plain sublet to My Listings

--- PennMobile/Subletting/Listings/MyListings.swift
+++ PennMobile/Subletting/Listings/MyListings.swift
@@ -1,3 +1,11 @@
+//
+//  MyListings.swift
+//  PennMobile
+//
+//  Created by Jordan H on 2/03/24.
+//  Copyright © 2024 PennLabs. All rights reserved.
+//
+
 import PennMobileShared
 import SwiftUI
 
@@ -30,6 +38,7 @@ class ListingsViewModel: ObservableObject {
 
 struct MyListings: View {
     @ObservedObject private var viewModel: ListingsViewModel
+    private var columns: [GridItem] = Array(repeating: .init(.flexible()), count: 2)
     
     init(viewModel: ListingsViewModel) {
         self.viewModel = viewModel
@@ -50,7 +59,13 @@ struct MyListings: View {
                 .padding()
                 
                 TabView(selection: $viewModel.tab) {
-                    Text("Tab Content 1").tabItem { Text("Tab Label 1") }
+                    ScrollView {
+                        LazyVGrid(columns: columns) {
+                            ForEach(viewModel.listings) { sublet in
+                                SubletItem(sublet: sublet)
+                            }
+                        }
+                    }
                         .tag(ListingsViewModel.Tab.posted)
                     Text("Tab Content 2").tabItem { Text("Tab Label 2") }
                         .tag(ListingsViewModel.Tab.drafts)
