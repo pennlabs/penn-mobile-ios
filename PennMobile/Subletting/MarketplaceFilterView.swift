dd273c8df62d66efb0491edaf4777afc2dd542b1
JHawk0224 <jhawk111@icloud.com>
2024-02-09 18:02:44 -0500

    Add some PennForms to filter page

--- PennMobile/Subletting/MarketplaceFilterView.swift
+++ PennMobile/Subletting/MarketplaceFilterView.swift
@@ -7,24 +7,73 @@
 //
 
 import SwiftUI
+import PennForms
 
 struct MarketplaceFilterView: View {
     @Environment(\.dismiss) var dismiss
+    @State private var minPrice: Int?
+    @State private var maxPrice: Int?
+    @State private var location: String?
+    @State private var startDate: Date?
+    @State private var endDate: Date?
+    @State private var beds: Int?
+    @State private var baths: Int?
+    @State private var selectedAmenities: [String] = []
+    let amenities = [
+        "Private bathroom", "In-unit laundry", "Gym", "Wifi",
+        "Walk-in closet", "Furnished", "Utilities included", "Swimming pool",
+        "Resident lounge", "Parking", "Patio", "Kitchen",
+        "Dog-friendly", "Cat-friendly"
+    ]
     
     var body: some View {
         NavigationStack {
-            Text("Hello, World!")
-                .navigationBarTitle(Text("Filter by"), displayMode: .inline)
-                .navigationBarItems(leading: Button(action: {
-                    dismiss()
-                }) {
-                    Image(systemName: "xmark")
-                        .foregroundColor(.black)
-                }, trailing: Button(action: {
-                    dismiss()
-                }) {
-                    Text("Save")
-                })
+            ScrollView {
+                LabsForm { formState in
+                    PairFields {
+                        NumericField($minPrice, format: .currency(code: "USD").presentation(.narrow), title: "Min Price/month")
+                        NumericField($maxPrice, format: .currency(code: "USD").presentation(.narrow), title: "Max Price/month")
+                    }
+                    
+                    TextLineField($location, placeholder: "Search", title: "Desired location")
+
+                    DateRangeField(lowerDate: $startDate, upperDate: $endDate, title: "Start & End Date")
+                    
+                    PairFields {
+                        NumericField($beds, title: "# Bed")
+                        NumericField($baths, title: "# Bath")
+                    }
+                    
+                    NumericField($baths, title: "# Bath")
+                    
+                    //TagSelector()
+                    
+                    ComponentWrapper {
+                        HStack {
+                            Button("Reset") {
+                                // Reset action
+                            }
+                            .frame(maxWidth: .infinity)
+                            
+                            Button("Save") {
+                                // Save action
+                            }
+                            .frame(maxWidth: .infinity)
+                        }
+                    }
+                }
+            }
+            .navigationBarTitle(Text("Filter by"), displayMode: .inline)
+            .navigationBarItems(leading: Button(action: {
+                dismiss()
+            }) {
+                Image(systemName: "xmark")
+                    .foregroundColor(.black)
+            }, trailing: Button(action: {
+                dismiss()
+            }) {
+                Text("Save")
+            })
         }
     }
 }
