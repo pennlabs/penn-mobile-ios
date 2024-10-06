96e58001d92d1914552526d1cf9c3d55d48f21ec
KKaay0708 <kaykaycsc@gmail.com>
2024-10-05 21:02:37 -0400

    add button to create view

---
+++ PennMobile/GSR-Booking/Controllers/quickBookingViewController.swift
@@ -0,0 +1,19 @@
+//
+//  quickBookingViewController.swift
+//  PennMobile
+//
+//  Created by Kaitlyn Kwan on 10/5/24.
+//  Copyright © 2024 PennLabs. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class QuickBookingViewController: UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .white
+        
+        
+    }
+}
