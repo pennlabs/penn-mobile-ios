db076d47d24bf0aa7d421621c7c95f487e0ad3c8
Jonathan Melitski <jonathanmelitski@gmail.com>
2025-03-22 18:44:04 -0400

    Rewrite GSRNetworkManager

--- PennMobile/GSR-Booking/Networking/GSRNetworkManager.swift
+++ PennMobile/GSR-Booking/Networking/GSRNetworkManager.swift
@@ -10,167 +10,104 @@ import Foundation
 import SwiftyJSON
 import PennMobileShared
 
-class GSRNetworkManager: NSObject, Requestable {
-
-    static let instance = GSRNetworkManager()
-
-    let availUrl = "https://pennmobile.org/api/gsr/availability/"
-    let locationsUrl = "https://pennmobile.org/api/gsr/locations/"
-    let bookingUrl = "https://pennmobile.org/api/gsr/book/"
-    let reservationURL = "https://pennmobile.org/api/gsr/reservations/"
-    let cancelURL = "https://pennmobile.org/api/gsr/cancel/"
-
-    func getLocations (completion: @escaping (Result<[GSRLocation], NetworkingError>) -> Void) {
-        let url = URL(string: self.locationsUrl)!
-
-        let task = URLSession.shared.dataTask(with: url) { (data, response, _) in
-            if let httpResponse = response as? HTTPURLResponse, let data = data, httpResponse.statusCode == 200 {
-
-                let decoder = JSONDecoder()
-                decoder.keyDecodingStrategy = .convertFromSnakeCase
-
-                do {
-                    var gsrLocations = try decoder.decode([GSRLocation].self, from: data)
-                    // Manually placing Huntsman as first row.
-                    // TODO: use analytics to decide on orders
-                    gsrLocations.sort(by: {a, b in a.gid < b.gid})
-                    completion(.success(gsrLocations))
-                } catch {
-                    completion(.failure(.parsingError))
-                }
-            }
-        }
-
-        task.resume()
-    }
-
-    func getAvailability(lid: String, gid: Int, startDate: String? = nil, endDate: String? = nil, completion: @escaping (Result<[GSRRoom], NetworkingError>) -> Void) {
-        var url = URL(string: "\(self.availUrl)")!
-        url.appendPathComponent(lid)
-        url.appendPathComponent("\(gid)")
-        
-        if let startDate = startDate {
-            url.appendQueryItem(name: "start", value: startDate)
-        }
-
-        if let endDate = endDate {
-            url.appendQueryItem(name: "end", value: endDate)
+class GSRNetworkManager {
+    static let availUrl = "https://pennmobile.org/api/gsr/availability/"
+    static let locationsUrl = "https://pennmobile.org/api/gsr/locations/"
+    static let bookingUrl = "https://pennmobile.org/api/gsr/book/"
+    static let reservationURL = "https://pennmobile.org/api/gsr/reservations/"
+    static let cancelURL = "https://pennmobile.org/api/gsr/cancel/"
+    
+    static func getLocations() async throws -> [GSRLocation] {
+        let url = URL(string: GSRNetworkManager.locationsUrl)!
+        let (data, response) = try await URLSession.shared.data(from: url)
+        guard let httpResponse = response as? HTTPURLResponse, httpResponse.statusCode == 200 else {
+            throw NetworkingError.serverError
         }
         
-        Task {
-            guard let request = try? await URLRequest(url: url, mode: .accessToken),
-                  let (data, response) = try? await URLSession.shared.data(for: request),
-                  let httpResponse = response as? HTTPURLResponse,
-                  httpResponse.statusCode == 200 else {
-                completion(.failure(.serverError))
-                return
-            }
-            
-            let decoder = JSONDecoder()
-            decoder.keyDecodingStrategy = .convertFromSnakeCase
-            decoder.dateDecodingStrategy = .iso8601
-            guard let gsrAvailability = try? decoder.decode(GSRAvailabilityAPIResponse.self, from: data) else {
-                completion(.failure(.parsingError))
-                return
-            }
-            
-            completion(.success(gsrAvailability.rooms))
+        let decoder = JSONDecoder()
+        decoder.keyDecodingStrategy = .convertFromSnakeCase
+        
+        return try decoder.decode([GSRLocation].self, from: data)
+    }
+    
+    static func getAvailability(for location: GSRLocation, startDate: Date? = nil, endDate: Date? = nil) async throws -> [GSRRoom] {
+        var url = URL(string: GSRNetworkManager.availUrl)!
+        url.appendPathComponent(location.lid)
+        url.appendPathComponent("\(location.gid)")
+        
+        let formatter = DateFormatter()
+        formatter.locale = Locale(identifier: "en_US_POSIX")
+        formatter.timeZone = TimeZone(abbreviation: "EST")!
+        formatter.dateFormat = "yyyy-MM-dd"
+        if let startDate {
+            url.appendQueryItem(name: "start", value: formatter.string(from: startDate))
+        }
+        if let endDate {
+            url.appendQueryItem(name: "end", value: formatter.string(from: endDate))
+        }
+        
+        let request = try await URLRequest(url: url, mode: .accessToken)
+        let (data, response) = try await URLSession.shared.data(for: request)
+        guard let httpResponse = response as? HTTPURLResponse, httpResponse.statusCode == 200 else {
+            throw NetworkingError.serverError
+        }
+        
+        let decoder = JSONDecoder()
+        decoder.keyDecodingStrategy = .convertFromSnakeCase
+        decoder.dateDecodingStrategy = .iso8601
+        
+        let res = try decoder.decode(GSRAvailabilityAPIResponse.self, from: data)
+        return res.rooms
+    }
+    
+    static func makeBooking(for booking: GSRBooking) async throws {
+        let url = URL(string: GSRNetworkManager.bookingUrl)!
+        var request = try await URLRequest(url: url, mode: .accessToken)
+        request.httpMethod = "POST"
+        request.addValue("application/json", forHTTPHeaderField: "Content-Type")
+
+        let dateFormatter = DateFormatter()
+        dateFormatter.locale = Locale(identifier: "en_US_POSIX")
+        dateFormatter.dateFormat = "yyyy-MM-dd'T'HH:mm:ssXXXXX"
+
+        let encoder = JSONEncoder()
+        encoder.keyEncodingStrategy = .convertToSnakeCase
+        encoder.dateEncodingStrategy = .formatted(dateFormatter)
+
+        request.httpBody = try encoder.encode(booking)
+        
+        let (data, response) = try await URLSession.shared.data(for: request)
+        guard let httpResponse = response as? HTTPURLResponse,
+              httpResponse.statusCode == 200 else {
+            throw NetworkingError.serverError
         }
     }
 
-    private func parseLocations(json: JSON) -> [Int: String] {
-        var locations: [Int: String] = [:]
-        if let jsonArray = json["locations"].array {
-            for json in jsonArray {
-                let id = json["id"].intValue
-                let name = json["name"].stringValue
-                locations[id] = name
-            }
+    static func getReservations() async throws -> [GSRReservation] {
+        let url = URL(string: GSRNetworkManager.reservationURL)!
+        let request = try await URLRequest(url: url, mode: .accessToken)
+        let (data, response) = try await URLSession.shared.data(for: request)
+        guard let httpResponse = response as? HTTPURLResponse,
+              httpResponse.statusCode == 200 else {
+            throw NetworkingError.serverError
         }
-        return locations
+        
+        let decoder = JSONDecoder()
+        decoder.keyDecodingStrategy = .convertFromSnakeCase
+        decoder.dateDecodingStrategy = .iso8601
+        return try decoder.decode([GSRReservation].self, from: data)
     }
 
-    func makeBooking(for booking: GSRBooking, _ completion: @escaping (Result<Void, NetworkingError>) -> Void) {
-        let url = URL(string: self.bookingUrl)!
-        Task {
-            guard var request = try? await URLRequest(url: url, mode: .accessToken) else {
-                completion(.failure(.authenticationError))
-                return
-            }
-            
-            request.httpMethod = "POST"
-            request.addValue("application/json", forHTTPHeaderField: "Content-Type")
-
-            let dateFormatter = DateFormatter()
-            dateFormatter.locale = Locale(identifier: "en_US_POSIX")
-            dateFormatter.dateFormat = "yyyy-MM-dd'T'HH:mm:ssXXXXX"
-
-            let encoder = JSONEncoder()
-            encoder.keyEncodingStrategy = .convertToSnakeCase
-            encoder.dateEncodingStrategy = .formatted(dateFormatter)
-
-            request.httpBody = try? encoder.encode(booking)
-            
-            guard let (data, response) = try? await URLSession.shared.data(for: request),
-                  let httpResponse = response as? HTTPURLResponse,
-                  httpResponse.statusCode == 200 else {
-                completion(.failure(.serverError))
-                return
-            }
-            
-            completion(.success(()))
-        }
-    }
-}
-
-// MARK: - Get Reservatoins
-extension GSRNetworkManager {
-    func getReservations(_ completion: @escaping (_ reservations: Result<[GSRReservation], NetworkingError>) -> Void) {
-        let url = URL(string: self.reservationURL)!
-        Task {
-            guard let request = try? await URLRequest(url: url, mode: .accessToken),
-                  let (data, response) = try? await URLSession.shared.data(for: request),
-                  let httpResponse = response as? HTTPURLResponse,
-                  httpResponse.statusCode == 200 else {
-                completion(.failure(.serverError))
-                return
-            }
-            
-            let decoder = JSONDecoder()
-            decoder.keyDecodingStrategy = .convertFromSnakeCase
-            decoder.dateDecodingStrategy = .iso8601
-            guard let reservations = try? decoder.decode([GSRReservation].self, from: data) else {
-                completion(.failure(.parsingError))
-                return
-            }
-            
-            completion(.success(reservations))
-        }
-    }
-}
-
-// MARK: - Delete Reservation
-extension GSRNetworkManager {
-    func deleteReservation(bookingId: String, _ completion: @escaping (Result<Void, NetworkingError>) -> Void ) {
-        let url = URL(string: self.cancelURL)!
-        Task {
-            guard var request = try? await URLRequest(url: url, mode: .accessToken) else {
-                completion(.failure(.authenticationError))
-                return
-            }
-            
-            request.setValue("application/json", forHTTPHeaderField: "Content-Type")
-            request.httpMethod = "POST"
-            request.httpBody = try? JSONSerialization.data(withJSONObject: ["booking_id": bookingId])
-            
-            guard let (data, response) = try? await URLSession.shared.data(for: request),
-                  let httpResponse = response as? HTTPURLResponse,
-                  httpResponse.statusCode == 200 else {
-                completion(.failure(.serverError))
-                return
-            }
-            
-            completion(.success(()))
+    static func deleteReservation(_ reservation: GSRReservation) async throws {
+        let url = URL(string: GSRNetworkManager.cancelURL)!
+        var request = try await URLRequest(url: url, mode: .accessToken)
+        request.setValue("application/json", forHTTPHeaderField: "Content-Type")
+        request.httpMethod = "POST"
+        request.httpBody = try JSONSerialization.data(withJSONObject: ["booking_id": reservation.bookingId])
+        
+        let (_, response) = try await URLSession.shared.data(for: request)
+        guard let httpResponse = response as? HTTPURLResponse, httpResponse.statusCode == 200 else {
+            throw NetworkingError.serverError
         }
     }
 }
