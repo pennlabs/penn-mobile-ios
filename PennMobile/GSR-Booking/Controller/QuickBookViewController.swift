0e4f0bc72a7e6524447418de388b51a0817ae8b4
Kaitlyn <kaykaycsc@gmail.com>
2025-09-02 13:16:36 -0400

    getting details logic

--- PennMobile/GSR-Booking/Controller/QuickBookViewController.swift
+++ PennMobile/GSR-Booking/Controller/QuickBookViewController.swift
@@ -8,50 +8,24 @@
 
 import SwiftUI
 
-class QuickBookViewController: UIViewController, ShowsAlert {
+class QuickBookViewController: UIViewController {
     
-    var locations: [GSRLocation] = GSRLocationModel.shared.getLocations()
-    fileprivate var prefList: [GSRLocation] = []
     fileprivate var location: GSRLocation!
-    
+    fileprivate var soonestDetails: (slot: GSRTimeSlot, room: GSRRoom)?
     fileprivate var soonestStartTimeString: String!
     fileprivate var soonestEndTimeString: String!
-    fileprivate var soonestTimeSlot: GSRTimeSlot!
-    fileprivate var soonestRoom: GSRRoom!
-    fileprivate var min: Date! = Date.distantFuture
-    
     fileprivate var allRooms: [GSRRoom]!
     
     override func viewDidLoad() {
         super.viewDidLoad()
     }
     
-    internal func setupSubmitAlert(location: GSRLocation){
-        let alert = UIAlertController(title: "Quick Book GSR", message: """
-            Soonest available GSR:
-            Time Slot: \(soonestStartTimeString!) to \(soonestEndTimeString!)
-            Room: \(soonestRoom.roomName)
-            Location: \(location.name)
-            
-            Quickly book the soonest available room in any location on campus.
-            """, preferredStyle: .alert)
-        
-        let cancelAction = UIAlertAction(title: "Cancel", style: .cancel) { _ in }
-        alert.addAction(cancelAction)
-        
-        let acceptAction = UIAlertAction(title: "Accept", style: .default) { _ in
-            self.quickBook()
-        }
-        
-        alert.addAction(acceptAction)
-        
-        DispatchQueue.main.async {
-            self.present(alert, animated: true, completion: nil)
-        }
-    }
-    
     internal func setupQuickBooking(location: GSRLocation, completion: @escaping () -> Void) {
         self.location = location
+        if let best = getSoonestTimeSlot() {
+            self.soonestDetails = best
+            completion()
+        }
         let group = DispatchGroup()
         var foundAvailableRoom = false
         
@@ -86,25 +60,26 @@ class QuickBookViewController: UIViewController, ShowsAlert {
 //        }
     }
     
-    private func getSoonestTimeSlot() {
-        
+    private func getSoonestTimeSlot() -> (slot: GSRTimeSlot, room: GSRRoom)? {
         let formatter = DateFormatter()
+        var current: (slot: GSRTimeSlot, room: GSRRoom)?
+        var start : Date! = .distantFuture
         formatter.timeZone = TimeZone.current
         formatter.dateFormat = "HH:mm"
         for room in allRooms {
             guard let availability = room.availability.first(where: { $0.startTime >= Date() }) else {
                 continue
             }
-            let startTime = availability.startTime
             
-            if startTime < min {
-                min = startTime
-                soonestStartTimeString = formatter.string(from: startTime)
+            if availability.startTime < start {
+                current = (availability, room)
+                start = availability.startTime
+                soonestStartTimeString = formatter.string(from: availability.startTime)
                 soonestEndTimeString = formatter.string(from: availability.endTime)
-                soonestTimeSlot = availability
-                soonestRoom = room
             }
         }
+
+        return current
     }
 }
 
@@ -113,7 +88,9 @@ extension QuickBookViewController: GSRBookable {
         if !Account.isLoggedIn {
             self.showAlert(withMsg: "You are not logged in!", title: "Error", completion: {self.navigationController?.popViewController(animated: true)})
         } else {
-            submitBooking(for: GSRBooking(gid: location.gid, startTime: soonestTimeSlot.startTime, endTime: soonestTimeSlot.endTime, id: soonestRoom.id, roomName: soonestRoom.roomName))
+            var timeSlot :GSRTimeSlot = soonestDetails!.slot
+            var timeRoom :GSRRoom = soonestDetails!.room
+            submitBooking(for: GSRBooking(gid: location.gid, startTime: timeSlot.startTime, endTime: timeSlot.endTime, id: timeRoom.id, roomName: timeRoom.roomName))
         }
     }
 }
