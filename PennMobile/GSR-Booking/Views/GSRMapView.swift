0912186cbf4997ecb72e405f4601590e70ffc182
Khoi Dinh <khoiddinh@gmail.com>
2025-10-04 14:52:44 -0400

    added map view

--- PennMobile/GSR-Booking/Views/GSRMapView.swift
+++ PennMobile/GSR-Booking/Views/GSRMapView.swift
@@ -6,3 +6,22 @@
 //  Copyright © 2025 PennLabs. All rights reserved.
 //
 
+import SwiftUI
+import MapKit
+
+struct GSRMapView : View {
+    @EnvironmentObject var vm: GSRViewModel
+    
+    @State var position = MapCameraPosition.region(
+        MKCoordinateRegion(
+            center: CLLocationCoordinate2D(latitude: 39.952468, longitude: -75.198336),
+            span: MKCoordinateSpan(latitudeDelta: 0.001, longitudeDelta: 0.001)
+        )
+    )
+    
+    var body : some View {
+        
+        Map(position: $position)
+            .ignoresSafeArea(.all)
+    }
+}
