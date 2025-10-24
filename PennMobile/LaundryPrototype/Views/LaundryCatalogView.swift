278a509732ae716fe3f7c11b150f1e479f87f350
Nathan Aronson <njaronson421@gmail.com>
2025-10-24 17:56:07 -0400

    Separated contenview from select view and began implementing laundryroomview

---
+++ PennMobile/LaundryPrototype/Views/LaundryCatalogView.swift
@@ -0,0 +1,19 @@
+//
+//  LaundryHallListView.swift
+//  PennMobile
+//
+//  Created by Nathan Aronson on 10/24/25.
+//  Copyright © 2025 PennLabs. All rights reserved.
+//
+
+import SwiftUI
+
+struct LaundryHallListView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    LaundryHallListView()
+}
