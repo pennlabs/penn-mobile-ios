f8554f87e35a33cc7ee2cb5e85659ee3290b9508
Ryan <ryan@coretsu.com>
2026-01-21 22:32:05 -0500

    fix for agh booking by adding authenticated request to get locations

--- PennMobile/GSR-Booking/Networking/GSRNetworkManager.swift
+++ PennMobile/GSR-Booking/Networking/GSRNetworkManager.swift
@@ -20,8 +20,8 @@ class GSRNetworkManager {
     
     static func getLocations() async throws -> [GSRLocation] {
         let url = URL(string: GSRNetworkManager.locationsUrl)!
-        let request = try await URLRequest(url: url, mode: .accessToken)
-        let (data, response) = try await URLSession.shared.data(for: request)
+        let request = URLRequest(url: url, cachePolicy: .reloadIgnoringLocalCacheData)
+        let (data, response) = try await URLSession(authenticationMode: .accessToken).data(for: request)
         guard let httpResponse = response as? HTTPURLResponse, httpResponse.statusCode == 200 else {
             throw NetworkingError.serverError
         }
