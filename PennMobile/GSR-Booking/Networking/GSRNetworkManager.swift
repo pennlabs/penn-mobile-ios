8f57c515a6af2adb45d5c5c07f4b3eac282c4e60
Anthony Li <antli@seas.upenn.edu>
2026-02-19 17:32:07 -0500

    Make GSRNetworkManager.getAvailability DST-aware

--- PennMobile/GSR-Booking/Networking/GSRNetworkManager.swift
+++ PennMobile/GSR-Booking/Networking/GSRNetworkManager.swift
@@ -39,7 +39,7 @@ class GSRNetworkManager {
         
         let formatter = DateFormatter()
         formatter.locale = Locale(identifier: "en_US_POSIX")
-        formatter.timeZone = TimeZone(abbreviation: "EST")!
+        formatter.timeZone = .nyc
         formatter.dateFormat = "yyyy-MM-dd"
         if let startDate {
             url.appendQueryItem(name: "start", value: formatter.string(from: startDate))
